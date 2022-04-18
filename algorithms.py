23898b4bf1d984ecc35137510fe03725f3579263
pringles8 <51466095+pringles8@users.noreply.github.com>
2022-04-18 12:58:04 -0400

    Genetic Algo Implemented
    Genetic Algo Implemented

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,7 @@ from game import WordleGame
 import random
 import math
 from itertools import product
+import string
 
 class BaseAlgorithm:
     '''
@@ -29,12 +30,12 @@ class BaseAlgorithm:
         Notes: I would make this so it updates based on a new root word then the possible words
         as branches or leafs.
 
-        it may look something like https://www.poirrier.ca/notes/wordle/ except more simple. 
+        it may look something like https://www.poirrier.ca/notes/wordle/ except more simple.
         recursion might be useful for this.
         '''
         # TO-DO
         pass
-    
+
     def make_guess(self) -> str:
         pass
 
@@ -68,7 +69,7 @@ class HumanAlgorithm(BaseAlgorithm):
                             all(word[i]==self.right_position[i] for i in self.right_position.keys()) \
                             and \
                             word not in self.guesses]
-        guess = random.choice(possible_words)   
+        guess = random.choice(possible_words)
         self.guesses.append(guess)
         return guess
 
@@ -98,7 +99,7 @@ class NaiveFrequencyAlgorithm(BaseAlgorithm):
         return countDict
 
     def make_guess(self,previous_guess=None) -> str:
-        
+
         if previous_guess==None:
             guess = random.choice(self.word_list)
             self.guesses.append(guess)
@@ -120,7 +121,7 @@ class NaiveFrequencyAlgorithm(BaseAlgorithm):
                             all(word[i]==self.right_position[i] for i in self.right_position.keys()) \
                             and \
                             word not in self.guesses]
-        
+
         frequency = self.countCharacterFrequency()
 
         max_aggregate = 0
@@ -135,7 +136,7 @@ class NaiveFrequencyAlgorithm(BaseAlgorithm):
                 guess = i
         self.guesses.append(guess)
         return guess
-        
+
 
 
 class MaxEntropyAlgorithm(BaseAlgorithm):
@@ -168,25 +169,25 @@ class MaxEntropyAlgorithm(BaseAlgorithm):
                 patterns[p]+=1
             else:
                 patterns[p]=1
-                
+
         entropy = 0
         for k,v in patterns.items():
             if v!=0:
                 prob = v/len(self.remaining_word_list)
                 info = -1 * math.log(prob,2)
                 entropy+=prob*info
-                
+
         return entropy
-    
+
 
     def make_guess(self,previous_guess=None) -> str:
-        
+
         if previous_guess==None:
             guess = random.choice(self.word_list)
             self.guesses.append(guess)
             return guess
-        
-        
+
+
         p_guess, squares = previous_guess
         for idx, square in enumerate(squares):
             if square=="GREY":
@@ -204,8 +205,8 @@ class MaxEntropyAlgorithm(BaseAlgorithm):
                             all(word[i]==self.right_position[i] for i in self.right_position.keys()) \
                             and \
                             word not in self.guesses]
-        
-        
+
+
         max_entropy = 0
         guess = random.choice(self.remaining_word_list)
 
@@ -214,18 +215,108 @@ class MaxEntropyAlgorithm(BaseAlgorithm):
             if(entropy>max_entropy):
                 max_entropy = entropy
                 guess = word
-            
-        
+
+
         self.guesses.append(guess)
         return guess
 
-    
-    
+class GeneticAlgortihm(BaseAlgorithm):
+    '''
+    Pick words by picking the first two guesses randomly. From here on out, loop - each word will be given a fitness,
+    selecting the 2 fittest, randomly combining them, and mutating each letter with some small probabilty.
+    '''
+
+    def __init__(self, word_list) -> None:
+        super().__init__(word_list)
+        self.fitness = []
+
+    def make_guess(self, previous_guess=None) -> str:
+        # If no previous guesses, make a random guess
+        if previous_guess == None:
+            guess = random.choice(self.word_list)
+            self.guesses.append(guess)
+            return guess
+
+        # If there is a previous guess, get infomration learned from it.
+        p_guess, squares = previous_guess
+        ind_fitness = 0
+
+        for idx, square in enumerate(squares):
+            if square=="GREY":
+                self.bad_letters.append(p_guess[idx])
+                ind_fitness -= 1
+            if square=="YELLOW" or square=="GREEN":
+                self.good_letters.append(p_guess[idx])
+                ind_fitness += 1
+            if square=="GREEN":
+                self.right_position[idx] = p_guess[idx]
+                ind_fitness -= 1
+                ind_fitness += 2
+
+        self.fitness.append(ind_fitness)
+
+        # If only one guess, we want to get a different guess from the first
+        if len(self.guesses) < 2:
+            guess = random.choice(self.word_list)
+            #bad_guess = True
+
+            while guess in self.guesses: #or bad_guess:
+                guess = random.choice(self.word_list)
+
+                #for letter_ind in len(range(guess)):
+                #    if guess[letter_ind] == self.right_position[letter_ind]:
+                #        bad_guess = False
+                #        continue
+                #    if guess[letter_ind] in self.bad_letters:
+                #        bad_guess = True
+                #        break
+            self.guesses.append(guess)
+            return guess
+
+        guess = 'not_in_list'
+
+        while guess not in self.word_list:
+            # Select the 2 fittest
+            inds = [0, 0]
+            max_fit = -6
+
+            for j in range(len(self.fitness)):
+                if self.fitness[j] > max_fit:
+                    max_fit = self.fitness[j]
+                    inds[0] = j
+
+            max_fit = -6
+            for j in range(len(self.fitness)):
+                if self.fitness[j] > max_fit and j != inds[0]:
+                    max_fit = self.fitness[j]
+                    inds[1] = j
+
+            guess1 = self.guesses[inds[0]]
+            guess2 = self.guesses[inds[1]]
+
+            # Cross over
+            inds = random.randint(0,4)
+            guess = guess1[:inds] + guess2[inds:]
+
+            # Mutate
+            for i in range(len(guess)):
+                if random.random() < 0.1:
+                    letter = random.choice(string.ascii_letters[:27])
+                    if i == 0:
+                        guess = letter + guess[1:]
+                    elif i == len(guess) - 1:
+                        guess = guess[:len(guess) - 1] + letter
+                    else:
+                        guess = guess[:i] + letter + guess[i+1:]
+
+        self.guesses.append(guess)
+        return guess
+
 class GreedyDepthAlgorithm(BaseAlgorithm):
     '''
     Pick the words by finding the path to minimal depth which basically minimizes the pool
     of possible words as the algorithm gets feedback.
-    
+
     Implemenation is inspired by: https://towardsdatascience.com/automatic-wordle-solving-a305954b746e
     '''
     def __init__(self, word_list, Verbose=False) -> None:
@@ -239,12 +330,12 @@ class GreedyBreadthAlgorithm(BaseAlgorithm):
     '''
     Similar to GreedyDepthAlgorithm but searches the word tree using breadth
     first search. I imagine this will be optimal since we would stop the search
-    once a leaf is found (which provides the shortest branch such that we would 
+    once a leaf is found (which provides the shortest branch such that we would
     want to pick the child of the root that brings us there).
     '''
     def __init__(self, word_list, Verbose=False) -> None:
         super().__init__(word_list, Verbose)
-    
+
     def make_guess(self) -> str:
         # TO-DO
         return super().make_guess()
@@ -264,8 +355,9 @@ if __name__=="__main__":
     print("Select an Algorithm:\
           \n1.Human Algorithm\
           \n2.Aggregated Frequency\
-          \n3.Entropy Maximization")
-    
+          \n3.Entropy Maximization\
+          \n4.Genetic Algorithm")
+
     choice = int(input())
     if choice == 1:
         algo = HumanAlgorithm(word_list=game.get_word_list())
@@ -273,6 +365,8 @@ if __name__=="__main__":
         algo = NaiveFrequencyAlgorithm(word_list=game.get_word_list())
     elif choice == 3:
         algo = MaxEntropyAlgorithm(word_list=game.get_word_list())
+    elif choice == 4:
+        algo = GeneticAlgortihm(word_list=game.get_word_list())
 
 
     #print("ANSWER:",game.answer)
